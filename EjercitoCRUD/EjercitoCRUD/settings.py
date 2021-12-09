74eaa8750161aa5bdd2411aa600bd269a5b05e60
Camilo Chaves <70001960+camichaves79@users.noreply.github.com>
2021-12-09 14:26:37 -0500

    Update settings.py

--- EjercitoCRUD/EjercitoCRUD/settings.py
+++ EjercitoCRUD/EjercitoCRUD/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ejercito', 
         'USER': 'postgres', 
-        'PASSWORD': '123PorMi!',
+        'PASSWORD': '********',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
